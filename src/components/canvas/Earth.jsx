810c02613ec563c2388685c6b65328bc75258e70
Basam Serad <basamserad1223@gmail.com>
2023-04-05 12:53:00 +0800

    Revert to javascript

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 export default function Earth() {
   return <div>Earth</div>;
 }
--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 export default function Stars() {
   return <div>Stars</div>;
 }
