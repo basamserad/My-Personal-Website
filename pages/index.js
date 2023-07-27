1da17921ff17ffc9c9f061e84285a956efaf474b
Basam Serad <basamserad1223@gmail.com>
2023-07-27 18:46:56 +0800

    Particles

--- pages/index.js
+++ pages/index.js
@@ -48,6 +48,7 @@ const Home = () => {
       {/* Particles And Image */}
       <div className="w-[1200px] h-full absolute right-0 bottom-0">
         <div className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0" />
+        <ParticlesContainer />
         <motion.div
           variants={fadeIn('up', 0.5)}
           initial="hidden"
