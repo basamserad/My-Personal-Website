8e9fe2bfd36badcd3b1f0cb18e020dd4eb12fa16
Basam Serad <basamserad1223@gmail.com>
2023-07-27 19:09:07 +0800

    Header

--- components/Socials.js
+++ components/Socials.js
@@ -1,5 +1,36 @@
-const Socials = () => {
-  return <div>Socials</div>;
-};
+import Link from 'next/link'
+import { FaGithub, FaLinkedinIn, FaFacebookMessenger } from 'react-icons/fa'
+import {SiGmail} from 'react-icons/si'
 
-export default Socials;
+const Socials = () => {
+  return (
+    <div className='flex items-center gap-x-5 text-2xl'>
+      <Link
+        href={''}
+        className="hover:text-accent transalation-all duration-300"
+      >
+        <SiGmail />
+      </Link>
+      <Link
+        href={''}
+        className="hover:text-accent transalation-all duration-300"
+      >
+        <FaGithub />
+      </Link>
+      <Link
+        href={''}
+        className="hover:text-accent transalation-all duration-300"
+      >
+        <FaLinkedinIn />
+      </Link>
+      <Link
+        href={''}
+        className="hover:text-accent transalation-all duration-300"
+      >
+        <FaFacebookMessenger />
+      </Link>
+    </div>
+  )
+}
+
+export default Socials
